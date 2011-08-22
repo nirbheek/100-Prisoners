ca6997fae1f689955dbcb7b10ab553b055709954
Nirbheek Chauhan <nirbheek.chauhan@gmail.com>
2011-08-22 23:01:38 +0530

    Merge the plain C and OpenMP versions
    * Convert while loop to for, other minor changes
    * Complex abstract mathematics from OpenMP #in-joke

--- 100_prisoners.c
+++ 100_prisoners.c
@@ -1,53 +1,67 @@
-// vim: sts=4 sw=4 et :
+/* vim: sts=4 sw=4 et :
+ * Author(s): Nirbheek Chauhan
+ * License: MIT
+ *
+ * Solve the 100 prisoners problem
+ * http://www.r-bloggers.com/100-prisoners-100-lines-of-code/
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <glib.h>
 
-static int num_inmates = 100;
-static int sample_size = 100;
+#define num_inmates 1000
+#define sample_size 10000
 
-int locate (int array[], int wanted) {
-    int count = 0;
+int locate (int array[], int wanted)
+{
+    int count;
     int found = array[wanted-1];
-    // Walk the boxes till we find 'wanted'
-    while (count < (num_inmates >> 1)) {
+    // Walk the boxes till we find 'wanted', or have checked half the boxes
+    for (count = 0; count < (num_inmates >> 1); count++) {
         if (found == wanted)
             return 1;
         found = array[found-1];
-        count++;
     }
     return 0;
 }
 
-int main() {
-    int samples_done = 0;
+int main()
+{
+    int i, samples_done;
     int success = 0;
     int failure = 0;
     int total_found = 0;
-    int inmate_success;
     int boxes[num_inmates];
-    int i, j;
     // Initialize boxes
     for (i = 0; i < num_inmates; i++)
-        boxes[i] = i+1;
-    srandom(time(NULL));
-    while (samples_done < sample_size) {
+        boxes[i] = i + 1;
+#ifdef OMP
+    #pragma omp parallel for reduction(+:total_found, success, failure) \
+        firstprivate(boxes) private(i)
+#endif
+    for (samples_done = 0; samples_done < sample_size; samples_done++) {
+        int inmate_success, j;
         // Shuffle boxes
+        srandom(time(NULL));
         for (i = num_inmates-1; i > 0; i--) {
-            //j = g_random_int_range (0, i+1);
             //j = (int) (i * (random() / (RAND_MAX + 1.0)));
-            j = random() % i; // i is a power of 10
+            j = random() % i; // CLOSE ENOUGH.
             if (i == j)
                 continue;
             boxes[i] = boxes[i] ^ boxes[j];
             boxes[j] = boxes[i] ^ boxes[j];
             boxes[i] = boxes[i] ^ boxes[j];
         }
+
         inmate_success = 0;
+#ifdef OMP
+        #pragma omp parallel for reduction(+:inmate_success) private(i)
+#endif
         for (i = 0; i < num_inmates; i++)
+            // Find the box with the number i+1 by looking at half the boxes
             inmate_success += locate(boxes, i+1);
-        samples_done++;
+
         total_found += inmate_success;
         if (inmate_success == num_inmates)
             success++;
--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
-all:
-	$(CC) -march=native -O3 -fopenmp 100_prisoners_openmp.c -o 100_omp_O3.o
-	$(CC) -march=native -O3 100_prisoners.c -o 100_O3.o
+all: 100_O3.o 100_omp_O3.o
 
-test:
+100_O3.o: 100_prisoners.c
+	$(CC) -ffast-math -march=native -O3 100_prisoners.c -o 100_O3.o
+
+100_omp_O3.o: 100_prisoners.c
+	$(CC) -ffast-math -march=native -O3 -fopenmp -DOMP 100_prisoners.c -o 100_omp_O3.o
+
+test: all
 	time ./100_O3.o;
 	time ./100_omp_O3.o;
 
 clean:
-	rm -f *.o
+	rm -f *.o a.out
